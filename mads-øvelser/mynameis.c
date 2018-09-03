34e7f0802642ad8f5e870a210a8bc65d48310cc4
kronborq <xlq446@alumni.ku.dk>
2018-09-03 14:55:23 +0200

    Upload af få øvelser

---
+++ mads-øvelser/mynameis.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+int main() {
+  printf("Mads\n");
+  return 0;
+}
---
+++ mads-øvelser/repeatme.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[]){
+
+  if (argc != 2){
+    printf("Wrong number of arguments\n");
+  }
+  else{
+    printf("%s\n", argv[1]);
+    printf("%s\n", argv[1]);
+  }
+  return 0;
+}
