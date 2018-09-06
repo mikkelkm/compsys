a2a42dd1de08a0c796fcfaa43c72d143a9637d91
kronborq <xlq446@alumni.ku.dk>
2018-09-06 21:30:52 +0200

    upload af filemads

--- Afleveringer/file.c
+++ Afleveringer/file.c
@@ -1,60 +1,37 @@
-#include <stdio.h>  // fprintf, stdout
-#include <stdlib.h> // EXIT_FAILURE, EXIT_SUCCESS.
-#include <string.h> // strings
+#include <stdio.h> // fprintf, stdout, stderr.
+#include <stdlib.h> // exit, EXIT_FAILURE, EXIT_SUCCESS.
+#include <string.h> // strerror.
+#include <errno.h> // errno.
+int main(int argc, char const *argv[]){
+  char file_type[256];
 
-int main(int argc, char* argv[]){
+  FILE *file = fopen(argv[1], "r");
+  int c = fgetc(file);
 
-	// we only accept exactly one argument
-	if (argc!=2) {return EXIT_FAILURE;}
+  if (argc != 2){
+    fprintf(stderr, "Usage: file path\n");
+    return EXIT_FAILURE;
+  }
+  else{
+    if (file == NULL){
+      fprintf(stdout, "%s: cannot determine (No such file or directory)\n", argv[1]);
+      return EXIT_SUCCESS;
+    }
 
-	// create file object
-	FILE *fp;
+    while(c != EOF){
+      printf("%d\n", c);
+      if(c > 0 && c < 127){
+        strcpy(file_type, "ASCII text");
+        return EXIT_SUCCESS;
+      }
+      else{
+        strcpy(file_type, "data");
+        return EXIT_SUCCESS;
+      }
+    }
+    fprintf(stdout, "%s: %s\n", argv[1], file_type);
+    return EXIT_SUCCESS;
+  }
 
-	// create char array object for first line of file
-	//char line[100];
-	// create char array object for name of file type
-	char file_type[20] = ".ASCII";
-	// create char for chars in file
-	int c;
-
-	// open file with first argument from input
-	fp = fopen(argv[1], "r");
-
-	// can we open file?
-	if (!fp) {
-		printf("Input read error!\n");
-		fclose(fp);
-		return EXIT_FAILURE;
-	} else {
-
-		// get first line
-		//if(fgets (line, 60, fp)!=NULL ) {
-//			puts(line);
-  // 		}
-
-		// move pointer to end of file
-		fseek(fp, 0, SEEK_END);
-
-    		int len_file = ftell(fp);
-		if(len_file<1){
-			strcpy(file_type, "EMPTY file");
-		}
-
-		// move pointer to start of file
-		fseek(fp, 0, SEEK_SET);
-
-		for(int i = 1;i<len_file;i++){
-			fscanf(fp, "%lc", &c);
-			//printf("%d\n", c);
-
-			if(c<1 || c>127){
-				strcpy(file_type, ".DATA");
-			}
-		}
-
-		printf("PATH: %s\n", file_type);
-		// close file
-   		fclose(fp);
-	}
-return EXIT_SUCCESS;
+  return 0;
 }
--- Afleveringer/fileMads.c
+++ Afleveringer/fileMads.c
@@ -3,25 +3,35 @@
 #include <string.h> // strerror.
 #include <errno.h> // errno.
 int main(int argc, char const *argv[]){
-  char file_path = argv[1];
-  char file_type;
+  char file_type[256];
 
-  FILE *f1 = fopen(argv[1], "r");
+  FILE *file = fopen(argv[1], "r");
+  int c = fgetc(file);
 
   if (argc != 2){
     fprintf(stderr, "Usage: file path\n");
     return EXIT_FAILURE;
   }
   else{
-    if (f1 == NULL){
-      fprintf(stderr, "%s%s cannot open %s (no such file or directory) \n", file_path,":", file_type);
-      return EXIT_FAILURE);
-    }
-    else{
-      fprintf(stdout, "%s: %s\n", file_path, file_type);
+    if (file == NULL){
+      fprintf(stdout, "%s: cannot determine (No such file or directory)\n", argv[1]);
       return EXIT_SUCCESS;
     }
 
+    while(c != EOF){
+      printf("%d\n", c);
+      if(c > 0 && c < 127){
+        strcpy(file_type, "ASCII text");
+        return EXIT_SUCCESS;
+      }
+      else{
+        strcpy(file_type, "data");
+        return EXIT_SUCCESS;
+      }
+    }
+    fprintf(stdout, "%s: %s\n", argv[1], file_type);
+    return EXIT_SUCCESS;
   }
+
   return 0;
 }
