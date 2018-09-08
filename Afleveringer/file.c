1a9ab69835e5eb8abd502a2faee6024f4f2b8c04
Christian  Baun <rbp111@alumni.ku.dk>
2018-09-08 08:30:55 +0200

    updated ascii for loop in file.c
    Har oversat hex krav til decimaltal og rettet vores loop, så det fanger de rigtige karakter. 
    I skal rette tests!
    
    Disse er ASCII
    07 - 13
    27
    32 - 126
    
    Alle andre tal er data.

--- Afleveringer/file.c
+++ Afleveringer/file.c
@@ -4,7 +4,7 @@
 #include <errno.h> // errors
 
 
-	// create enum class with file types and string output
+	// create enum class with file types
 	enum file_type {
 	  DATA,
 	  EMPTY,
@@ -15,7 +15,8 @@ int main(int argc, char* argv[]){
 
 	// we only accept exactly one argument
 	if (argc!=2) {return EXIT_FAILURE;}
-
+	
+	// string output to assist enum class
 	const char * const FILE_TYPE_STRINGS[] = {
 	  "data",
 	  "empty",
@@ -25,16 +26,17 @@ int main(int argc, char* argv[]){
 	// set default file type
 	enum file_type tp;
 	tp = ASCII;
-
+	
 	// create file object
 	FILE *fp;
 
 	// create char for chars in file
+	///// if this variable is not initialized, then the program does not work with gcc, but does with clang
 	int c = 0;
 
 	// open file with first argument from input
 	fp = fopen(argv[1], "rb");
-
+	
 	// can we open file?
 	if (!fp) {
 		printf("Input read error!\n");
@@ -45,37 +47,34 @@ int main(int argc, char* argv[]){
     		int len_file = ftell(fp);
 
 		// EOF < 1 then the file is empty
-		if(len_file<1){
+		if(len_file==0){
 		  tp = EMPTY;
 		}
 
 		// move pointer to start of file
 		fseek(fp, 0, SEEK_SET);
-
+		
 		// for loop to check chars in file
 		for(int i = 1;i<len_file;i++){
 
 		  // use fscanf(1) or fread(1)?
 		  //fscanf(fp, "%lc", &c);
+		  fread(&c,sizeof(char),1,fp);
 
-		  fread(&c, sizeof(char),1,fp);
-
-			// test print!
-      //printf("hej: %ld \n",ftell(fp));
-			//printf("%d = %c\n", c, c);
-      //printf(" %lc = %d = %c\n", c, c, c);
-
-			// test if file data differs from ASCII
-			if(c<1 || c>127){
-			  tp = DATA;
-			}
+		  // test print!
+		  // printf("%d = %c\n", c, c);
+			
+		  // test if file data differs from the ASCII characters mentioned in assignment description
+		  if(!( (c>=7 && c<=13) || (c==27) || (c>=32 && c<=126) )){
+		    tp = DATA;
+		    break;
+		  }
 		}
 		// print result of data file analysis
 		printf("%s: %s\n", argv[1],  FILE_TYPE_STRINGS[tp]);
-
+		
 		// close file
    		fclose(fp);
 	}
 return EXIT_SUCCESS;
 }
-
