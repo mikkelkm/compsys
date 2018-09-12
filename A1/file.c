584ba19550dfefb84b048114951f48799750f705
Baunbaunbaun <baunbaun@gmail.com>
2018-09-12 20:27:11 +0200

    update of file. new architecture

--- A1/file.c
+++ A1/file.c
@@ -9,12 +9,12 @@
 	  DATA,
 	  EMPTY,
 	  ASCII,
-	  UTF-8,
+	  UTF_8,
 	  Little_UTF_16,
 	  Big_UTF_16	  
 	};
 
-void detect_and_print_file_type(FILE *fp){
+void detect_and_print_file_type(char *path){
 
 	// string output to assist enum class
 	const char * const FILE_TYPE_STRINGS[] = {
@@ -26,72 +26,61 @@ void detect_and_print_file_type(FILE *fp){
 	  "Big-endian UTF-16 Unicode text"
 	};
 
+	// create char for chars in file
+	int c = 0;
+	
 	// set default file type
 	enum file_type tp = ASCII;
 
-
-  
-
-  	}
-  
-
-
-int main(int argc, char* argv[]){
-
 	// create file object
 	FILE *fp;
-
-	// create char for chars in file
-	int c = 0;
-
-	// open files with arguments from input
-	for(int i = 1;i < argc;i++){
-	  fp = fopen(argv[i], "rb");
-
-	  // can we open all file?
-	  if (!fp) {
-	   printf("Input read error!\n");
-	   return EXIT_FAILURE;
-	}
+	fp = fopen(path, "rb");
 	  
+	// can we open file?
+	if (!fp) {
+	  printf("Input read error!\n");
+	} else {
+	  // get length of file
+	  fseek(fp, 0, SEEK_END);
+	  int len_file = ftell(fp);
+
+	  // EOF < 1 then the file is empty
+	  if(len_file==0){
+	    tp = EMPTY;
+	  }
+
+	  // move pointer to start of file
+	  fseek(fp, 0, SEEK_SET);
+	  
+	  // for loop to check chars in file
+	  for(int i = 1;i<len_file;i++){
+	    // use fscanf(1) or fread(1)?
+	    //fscanf(fp, "%lc", &c);
+	    fread(&c,sizeof(char),1,fp);
+	    
+	    // test print!
+	    // printf("%d = %c\n", c, c);
+	    
+	    // test if file data differs from the ASCII characters mentioned in assignment description
+	    if(!( (c>=7 && c<=13) || (c==27) || (c>=32 && c<=126) )){
+	      tp = DATA;
+	      break;
+	    }
+	  }
 	}
+	// close file
+	fclose(fp);
 	
-
-
-	 else {
-		// move pointer to end of file and get value
-		fseek(fp, 0, SEEK_END);
-    		int len_file = ftell(fp);
-
-		// EOF < 1 then the file is empty
-		if(len_file==0){
-		  tp = EMPTY;
-		}
-
-		// move pointer to start of file
-		fseek(fp, 0, SEEK_SET);
-
-		// for loop to check chars in file
-		for(int i = 1;i<len_file;i++){
-
-		  // use fscanf(1) or fread(1)?
-		  //fscanf(fp, "%lc", &c);
-		  fread(&c,sizeof(char),1,fp);
-
-		  // test print!
-		  // printf("%d = %c\n", c, c);
-
-		  // test if file data differs from the ASCII characters mentioned in assignment description
-		  if(!( (c>=7 && c<=13) || (c==27) || (c>=32 && c<=126) )){
-		    tp = DATA;
-		    break;
-		  }
-		}
-		// print result of data file analysis
-		printf("%s: %s\n", argv[1],  FILE_TYPE_STRINGS[tp]);
-
-		// close file
-   		fclose(fp);
-	}
+	// print result of file analysis
+	printf("%s: %s\n", path, FILE_TYPE_STRINGS[tp]);
+}
+  
+int main(int argc, char *argv[]){
+  
+	// check each argument
+	for(int i = 1;i < argc;i++){
+	  // detect file type of path i
+	  detect_and_print_file_type(argv[i]);
+	}	
 return EXIT_SUCCESS;
 }
