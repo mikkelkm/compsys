55603bb91d69891b17143cde00dbb8ec25416c26
kronborq <xlq446@alumni.ku.dk>
2018-11-10 18:35:17 +0100

    Kommentarer og indentering

--- A4/src/transducers.c
+++ A4/src/transducers.c
@@ -8,149 +8,135 @@ struct stream {
   int flag;
 };
 
-// hint from assigment
+//Hint from assignment
+//Creates FILE* objects from pipes
 static int file_pipe(FILE* files[2]) {
-    int fds[2];
+  int fds[2];
 
-    int r = pipe(fds);
+  int r = pipe(fds);
 
-    if (r == 0) {
-        files[0] = fdopen(fds[0], "r");
-        files[1] = fdopen(fds[1], "w");
+  if (r == 0) {
+    files[0] = fdopen(fds[0], "r");
+    files[1] = fdopen(fds[1], "w");
 
-        if (files[0] && files[1]) {
-            return 0;
-        } else {
-            return 1;
-        }
+    if (files[0] && files[1]) {
+      return 0;
     } else {
-        return r;
+      return 1;
     }
+  } else {
+    return r;
+  }
 }
 
-
-
-
-
+//Self-explanatory, frees the stream when needed
 void transducers_free_stream(stream *s){
   fclose(s->file);
   free(s);
 }
 
-
-
+//Transducer used to create a stream
 int transducers_link_source(stream **out,
-                            transducers_source s,
-                            const void *arg) {
+                            transducers_source s,const void *arg) {
 
-        // file in and out
-    FILE* files[2];
-        // open files and set to read only and write only
-    assert(file_pipe(files)==0);
-    pid_t p = fork();
+  FILE* files[2];
+  assert(file_pipe(files)==0);
+  pid_t ret = fork();
 
-        //child process
-    if (p == 0) {
-            //close pipe file in
-        assert(fclose(files[0])==0);
-            //put arg in pipe out file
-        s(arg, files[1]);
-        exit(0);
-    }
-    else {
-            // close pipe out file
-        assert(fclose(files[1])==0);
-            // allocate mem for a stream
-        stream * str = malloc(sizeof(stream));
-            // initiate values in struct
-        str -> file = files[0];
-        str -> flag = 0;
-        *out = str ;
-        return 0;
-    }
-    return 1;
-}
-
-
-int transducers_link_sink(transducers_sink s,
-                          void *arg,
-                          stream *in) {
-
-        // does the stream have a reader?
-    if (in -> flag == 0) {
-        in -> flag = 1;
-        s(arg, in -> file);
-        return 0;
-    }
-    return 1;
-}
-
-int transducers_link_1(stream **out,
-                       transducers_1 t,
-                       const void *arg,
-                       stream* in) {
-
-    if (in -> flag == 0) {
-        FILE* files[2];
-        assert(file_pipe(files)==0);
-        pid_t p = fork();
-
-        if (p == 0) {
-            in -> flag = 1;
-            assert(fclose(files[0])==0);
-            t(arg, files[1], in -> file);
-            exit(0);
-        }
-        else {
-            assert(fclose(files[1])==0);
-            stream * str = malloc(sizeof(stream));
-            str -> file = files[0];
-            str -> flag = 0;
-            *out = str ;
-        }
-        return 0;
-    }
-    return 1;
-}
-
-int transducers_link_2(stream **out,
-                       transducers_2 t,
-                       const void *arg,
-                       stream* in1,
-                       stream* in2) {
-
-    if (in1 -> flag == 0) {
-      FILE* pipe_ports[3];
-      assert(file_pipe(pipe_ports)==0);
-      pid_t p = fork();
-
-      if (p == 0) {
-        in1 -> flag = 1;
-        in2 -> flag = 1;
-        assert(fclose(pipe_ports[0])==0);
-        t(arg, pipe_ports[2], in1, in2 -> file);
-        }
-      else {
-        assert(fclose(pipe_ports[1])==0);
-        for (size_t i = 0; i < 2; i++) {
-          stream * str = malloc(sizeof(stream));
-          str -> file = pipe_ports[i];
-          str -> flag = 0;
-          *out = str ;
-        }
-      }
-      return 0;
+  if (ret == 0) {
+    assert(fclose(files[0])==0);
+    s(arg, files[1]);
+    return 0;
+  }
+  else {
+    assert(fclose(files[1])==0);
+    stream * str = malloc(sizeof(stream)); //Allocating memory for the stream
+    str -> file = files[0];
+    str -> flag = 0;
+    *out = str ;
+    return 0;
   }
   return 1;
 }
 
-//Virker ikke endnu
-int transducers_dup(stream **out1,
-                    stream **out2,
-                    stream *in) {
+/* Transducer which uses the flag from our stream struct, to block
+untill the transducer function returns. */
+int transducers_link_sink(transducers_sink s, void *arg, stream *in) {
 
+  if (in -> flag == 0) {
+    in -> flag = 1;
+    s(arg, in -> file);
+    return 0;
+  }
+  return 1;
+}
+
+// Transducer used to work between 1 input stream and 1 output stream
+int transducers_link_1(stream **out,
+                       transducers_1 t, const void *arg,
+                       stream* in) {
+
+  if (in -> flag == 0) {
+    FILE* files[2];
+    assert(file_pipe(files)==0);
+    pid_t ret = fork(); //Generates a child and a parent process
+
+    //Child process which works on the input
+    if (ret == 0) {
+      in -> flag = 1;
+      assert(fclose(files[0])==0);
+      t(arg, files[1], in -> file);
+    }
+
+    //Parent process which works on the output
+    else {
+      assert(fclose(files[1])==0);
+      stream * str = malloc(sizeof(stream));
+      str -> file = files[0];
+      str -> flag = 0;
+      *out = str ;
+    }
+    return 0;
+  }
+  return 1;
+}
+
+// Transducer used to work between 2 input streams and 1 output stream
+int transducers_link_2(stream **out,
+                       transducers_2 t, const void *arg,
+                       stream* in1, stream* in2) {
+
+  if (in1 -> flag == 0) {
+    FILE* pipe_ports[3];
+    assert(file_pipe(pipe_ports)==0);
+    pid_t ret = fork();
+
+    if (ret == 0) {
+      in1 -> flag = 1;
+      in2 -> flag = 1;
+      assert(fclose(pipe_ports[0])==0);
+      //write both inputs to single FILE*
+      t(arg, pipe_ports[2], in1, in2 -> file);
+    }
+    else {
+      assert(fclose(pipe_ports[1])==0);
+      for (size_t i = 0; i < 2; i++) {
+        stream * str = malloc(sizeof(stream));
+        str -> file = pipe_ports[i];
+        str -> flag = 0;
+        *out = str ;
+      }
+    }
+    return 0;
+  }
+  return 1;
+}
+
+//Transducer to duplicate an input stream, into two differnet output streams
+int transducers_dup(stream **out1,stream **out2, stream *in) {
 
   if (in -> flag == 0){
+    //Generating a FILE* twice, so we can access both output streams
     FILE* files1[2];
     assert(file_pipe(files1) == 0);
     FILE* files2[2];
@@ -159,27 +145,26 @@ int transducers_dup(stream **out1,
 
     assert(p >= 0);
 
-if (p == 0) {
+    if (p == 0) {
 
-  char c;
-  while (fread(&c, sizeof(char), 1, in -> file) == 1 )
-  {
-    fwrite(&c, sizeof(char), 1, files1[1]);
-    fwrite(&c, sizeof(char), 1, files2[1]);
-  }
-  exit(0);
-}
-
-
-assert(fclose(files1[1])==0);
-assert(fclose(files2[1])==0);
-stream * str1 = malloc(sizeof(stream));
-stream * str2 = malloc(sizeof(stream));
-str1 -> file = files1[0];
-str2 -> file = files2[0];
-*out1 = str1 ;
-*out2 = str2 ;
-return 0;
+      char c;
+      while (fread(&c, sizeof(char), 1, in -> file) == 1 )
+      {
+        fwrite(&c, sizeof(char), 1, files1[1]);
+        fwrite(&c, sizeof(char), 1, files2[1]);
+      }
+      exit(0);
+    }
+    //Doing everything twice, once for every output stream
+    assert(fclose(files1[1])==0);
+    assert(fclose(files2[1])==0);
+    stream * str1 = malloc(sizeof(stream));
+    stream * str2 = malloc(sizeof(stream));
+    str1 -> file = files1[0];
+    str2 -> file = files2[0];
+    *out1 = str1 ;
+    *out2 = str2 ;
+    return 0;
   }
   return 1;
 
