6727fdc347b215c5e11a2bfbb9c7f94ab09d1043
kronborq <xlq446@alumni.ku.dk>
2018-11-10 15:20:19 +0100

    upload af link2 og start på dup

--- A4/src/transducers.c
+++ A4/src/transducers.c
@@ -13,15 +13,15 @@ void transducers_free_stream(stream *s){
 }
 
 // hint from assigment
-static int file_pipe(FILE* files[2]) { 
+static int file_pipe(FILE* files[2]) {
     int fds[2];
-    
+
     int r = pipe(fds);
-    
+
     if (r == 0) {
         files[0] = fdopen(fds[0], "r");
         files[1] = fdopen(fds[1], "w");
-        
+
         if (files[0] && files[1]) {
             return 0;
         } else {
@@ -36,11 +36,11 @@ static int file_pipe(FILE* files[2]) {
 int transducers_link_source(stream **out,
                             transducers_source s,
                             const void *arg) {
-    
+
     FILE* pipe_ports[2];
     assert(file_pipe(pipe_ports)==0);
     pid_t ret = fork();
-    
+
     if (ret == 0) {
         assert(fclose(pipe_ports[0])==0);
         s(arg, pipe_ports[1]);
@@ -61,12 +61,12 @@ int transducers_link_source(stream **out,
 int transducers_link_sink(transducers_sink s,
                           void *arg,
                           stream *in) {
-    
+
     if (in -> flag == 0) {
         in -> flag = 1;
         s(arg, in -> file);
         return 0;
-    } 
+    }
     return 1;
 }
 
@@ -74,12 +74,12 @@ int transducers_link_1(stream **out,
                        transducers_1 t,
                        const void *arg,
                        stream* in) {
-    
+
     if (in -> flag == 0) {
         FILE* pipe_ports[2];
         assert(file_pipe(pipe_ports)==0);
         pid_t ret = fork();
-      
+
         if (ret == 0) {
             in -> flag = 1;
             assert(fclose(pipe_ports[0])==0);
@@ -103,20 +103,44 @@ int transducers_link_2(stream **out,
                        stream* in1,
                        stream* in2) {
 
-  out=out; /* unused */
-  t=t; /* unused */
-  arg=arg; /* unused */
-  in1=in1; /* unused */
-  in2=in2; /* unused */
+    if (in1 -> flag == 0) {
+      FILE* pipe_ports[3];
+      assert(file_pipe(pipe_ports)==0);
+      pid_t ret = fork();
+
+      if (ret == 0) {
+        in1 -> flag = 1;
+        in2 -> flag = 1;
+        assert(fclose(pipe_ports[0])==0);
+        t(arg, pipe_ports[2], in1, in2 -> file);
+        }
+      else {
+        assert(fclose(pipe_ports[1])==0);
+        for (size_t i = 0; i < 2; i++) {
+          stream * str = malloc(sizeof(stream));
+          str -> file = pipe_ports[i];
+          str -> flag = 0;
+          *out = str ;
+        }
+      }
+      return 0;
+  }
   return 1;
 }
 
+//Virker ikke endnu
 int transducers_dup(stream **out1,
                     stream **out2,
                     stream *in) {
-    
-    out1=out1; /* unused */
-    out2=out2; /* unused */
-    in=in; /* unused */
-    return 1;
+
+  if (in -> flag == 0){
+    FILE* pipe_ports1[2];
+    assert(file_pipe(pipe_ports1) == 0);
+    FILE* pipe_ports2[2];
+    assert(file_pipe(pipe_ports2) == 0);
+    pid_t ret = fork();
+  }
+
+
+  return 1;
 }
